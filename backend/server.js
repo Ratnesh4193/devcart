de3f28e9a248b72f4eb0900d188a9eaee24eea69
Ratnesh <rktssgk@gmail.com>
2023-04-30 21:10:12 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,6 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('backend/build'))
-    const path = require('path')
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'backend','build','index.html'))
     })
