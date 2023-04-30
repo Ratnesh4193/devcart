ca1b2782411f615db0722e639d8af4a7eb1b35a8
Ratnesh <rktssgk@gmail.com>
2023-04-30 23:15:48 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static('backend/build'))
+    app.use(express.static('frontend/build'))
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'backend','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
     })
 } else {
 	app.get("/", (req, res) => {
