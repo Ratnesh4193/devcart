485e049179a263ff0bd6e0fda4c5989e4533d96e
Ratnesh <rktssgk@gmail.com>
2023-04-30 20:56:47 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -26,12 +26,12 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-	);
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('backend/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'backend','build','index.html'))
+    })
 } else {
 	app.get("/", (req, res) => {
 		res.send("API is running....");
